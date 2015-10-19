3bf189b93543cf7963acdeeb77e14d75299f5fd9
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-19 14:46:01 -0700

    updated readme, added more info to docstrings

--- README.md
+++ README.md
@@ -5,19 +5,51 @@ habanero
 
 This is a low level client for working with Crossref's search API. It's been named to be more generic, as other organizations are/will adopt Crossref's search API, making it possible to ineract with all from one client. 
 
+Crossref API docs: [https://github.com/CrossRef/rest-api-doc/blob/master/rest_api.md](https://github.com/CrossRef/rest-api-doc/blob/master/rest_api.md)
+
+Other Crossref API clients:
+
+- Ruby: [serrano](https://github.com/sckott/serrano)
+- R: [rcrossref](https://github.com/ropensci/rcrossref)
+
 ### Installation
 
 ```
 sudo pip install git+git://github.com/sckott/habanero.git#egg=habanero
 ```
 
-```python
-import habanero
+OR 
+
+```
+git clone git@github.com:sckott/habanero.git
+cd habanero
+make install
 ```
 
 ### Usage
 
-...
+Initialize
+
+```python
+from habanero import Habanero
+hb = Habanero()
+```
+
+Works route
+
+```python
+x = hb.works(query = "ecology")
+x.status()
+x.message()
+x.total_results()
+x.items()
+```
+
+Members route
+
+```python
+hb.members(ids = 98, works = True)
+```
 
 ### License
 
--- habanero/habanero.py
+++ habanero/habanero.py
@@ -8,6 +8,10 @@ class Habanero(object):
     '''
     Habanero: Main habanero class
 
+    Includes methods matching Crossref API routes:
+    - /works
+    - /members
+
     >>> from habanero import Habanero
     >>>
     >>> hb = Habanero()
@@ -31,14 +35,38 @@ class Habanero(object):
         '''
         Search Crossref works
 
-        :param ids: dois.
+        :param ids: [Array] DOIs (digital object identifier) or other identifiers
+        :param query: [String] A query string
+        :param filter: [Hash] Filter options. See ...
+        :param offset: [Fixnum] Number of record to start at, from 1 to infinity.
+        :param limit: [Fixnum] Number of results to return. Not relavant when searching with specific dois. Default: 20. Max: 1000
+        :param sample: [Fixnum] Number of random results to return. when you use the sample parameter,
+        the limit and offset parameters are ignored.
+        :param sort: [String] Field to sort on, one of score, relevance,
+        updated (date of most recent change to metadata. Currently the same as deposited),
+        deposited (time of most recent deposit), indexed (time of most recent index), or
+        published (publication date). Note: If the API call includes a query, then the sort
+        order will be by the relevance score. If no query is included, then the sort order
+        will be by DOI update date.
+        :param order: [String] Sort order, one of 'asc' or 'desc'
+        :param facet: [Boolean] Include facet results. Default: false
+
+        :return: Object response class, light wrapper around a dict
 
         Usage
         >>> from habanero import Habanero
         >>> hb = Habanero()
         >>> hb.works()
         >>> hb.works(ids = '10.1371/journal.pone.0033693')
-        >>> hb.works(query = "ecology")
+        >>> x = hb.works(query = "ecology")
+        >>> x.status()
+        >>> x.message_type()
+        >>> x.message_version()
+        >>> x.message()
+        >>> x.total_results()
+        >>> x.items_per_page()
+        >>> x.query()
+        >>> x.items()
         '''
         res = request(self.base_url, "/works/", ids,
           query, filter, offset, limit, sample, sort,
@@ -51,7 +79,22 @@ class Habanero(object):
         '''
         Search Crossref members
 
-        :param ids: member ids.
+        :param ids: [Array] DOIs (digital object identifier) or other identifiers
+        :param query: [String] A query string
+        :param filter: [Hash] Filter options. See ...
+        :param offset: [Fixnum] Number of record to start at, from 1 to infinity.
+        :param limit: [Fixnum] Number of results to return. Not relavant when searching with specific dois. Default: 20. Max: 1000
+        :param sample: [Fixnum] Number of random results to return. when you use the sample parameter,
+        the limit and offset parameters are ignored.
+        :param sort: [String] Field to sort on, one of score, relevance,
+        updated (date of most recent change to metadata. Currently the same as deposited),
+        deposited (time of most recent deposit), indexed (time of most recent index), or
+        published (publication date). Note: If the API call includes a query, then the sort
+        order will be by the relevance score. If no query is included, then the sort order
+        will be by DOI update date.
+        :param order: [String] Sort order, one of 'asc' or 'desc'
+        :param facet: [Boolean] Include facet results. Default: false
+        :param works: [Boolean] If true, works returned as well. Default: false
 
         Usage
         >>> from habanero import Habanero
