1d2ccdc0b3beb9994aaa5c7d889a54d5574c2f93
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-26 17:06:58 -0700

    added some exampels to works method for processing output #10

--- habanero/habanero.py
+++ habanero/habanero.py
@@ -76,6 +76,16 @@ class Habanero(object):
         >>> x = hb.works(filter = {'has_full_text': True})
         >>> x.links()
         >>>
+        >>> # Parse output to various data pieces
+        >>> x = hb.works(filter = {'has_full_text': True})
+        >>> ## get doi for each item
+        >>> [ z['DOI'] for z in x.result['message']['items'] ]
+        >>> ## get doi and url for each item
+        >>> [ {"doi": z['DOI'], "url": z['URL']} for z in x.result['message']['items'] ]
+        >>> ### print every doi
+        >>> for i in x.result['message']['items']:
+        >>>      print i['DOI']
+        >>>
         >>> # filters - pass in as a dict
         >>> ## see https://github.com/CrossRef/rest-api-doc/blob/master/rest_api.md#filter-names
         >>> hb.works(filter = {'has_full_text': True})
