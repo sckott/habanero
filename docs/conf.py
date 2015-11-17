5b6ee526ef45070c819ab33bbafddce67b52224d
Scott Chamberlain <myrmecocystus@gmail.com>
2015-11-17 08:32:28 -0500

    update version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ author = u'Scott Chamberlain'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0.6'
+version = '0.1.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
