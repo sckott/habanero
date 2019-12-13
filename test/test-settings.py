0790a4ef9bc01df532d44cca47741c507dbeedc9
Scott Chamberlain <myrmecocystus@gmail.com>
2019-12-12 16:42:59 -0800

    #84 add additional tests ua_string

--- test/test-settings.py
+++ test/test-settings.py
@@ -12,7 +12,7 @@ cr_with_bad_ua = Crossref(ua_string = 5)
 vcr_path = 'test/vcr_cassettes/setting_ua_string.yaml'
 @vcr.use_cassette(vcr_path)
 def test_ua_string():
-    "settings (ua_string) - with ua string"
+    "settings (ua_string) - with ua string, works"
     res = cr_with_ua.works(ids = '10.1371/journal.pone.0033693')
     x = open(vcr_path, "r").read()
     xy = yaml.safe_load(x)
@@ -24,7 +24,7 @@ def test_ua_string():
 vcr_noua_path = 'test/vcr_cassettes/setting_no_ua_string.yaml'
 @vcr.use_cassette(vcr_noua_path)
 def test_no_ua_string():
-    "settings (ua_string) - without ua string"
+    "settings (ua_string) - without ua string, works"
     res = cr_without_ua.works(ids = '10.1371/journal.pone.0033693')
     x = open(vcr_noua_path, "r").read()
     xy = yaml.safe_load(x)
@@ -32,6 +32,42 @@ def test_no_ua_string():
     
     assert 'foo bar' not in heads['User-Agent'][0]
     assert 'foo bar' not in heads['X-USER-AGENT'][0]
+
+vcr_path_members = 'test/vcr_cassettes/setting_ua_string_members.yaml'
+@vcr.use_cassette(vcr_path_members)
+def test_ua_string_members():
+    "settings (ua_string) - with ua string, members"
+    res = cr_with_ua.members(query = "ecology", limit = 2)
+    x = open(vcr_path_members, "r").read()
+    xy = yaml.safe_load(x)
+    heads = xy['interactions'][0]['request']['headers']
+    
+    assert 'foo bar' in heads['User-Agent'][0]
+    assert 'foo bar' in heads['X-USER-AGENT'][0]
+
+vcr_path_prefixes = 'test/vcr_cassettes/setting_ua_string_prefixes.yaml'
+@vcr.use_cassette(vcr_path_prefixes)
+def test_ua_string_prefixes():
+    "settings (ua_string) - with ua string, prefixes"
+    res = cr_with_ua.prefixes(ids = "10.1016", works = True, sample = 2)
+    x = open(vcr_path_prefixes, "r").read()
+    xy = yaml.safe_load(x)
+    heads = xy['interactions'][0]['request']['headers']
+    
+    assert 'foo bar' in heads['User-Agent'][0]
+    assert 'foo bar' in heads['X-USER-AGENT'][0]
+
+vcr_path_registration_agency = 'test/vcr_cassettes/setting_ua_string_registration_agency.yaml'
+@vcr.use_cassette(vcr_path_registration_agency)
+def test_ua_string_registration_agency():
+    "settings (ua_string) - with ua string, registration_agency"
+    res = cr_with_ua.registration_agency(u'10.1126/science.169.3946.635')
+    x = open(vcr_path_registration_agency, "r").read()
+    xy = yaml.safe_load(x)
+    heads = xy['interactions'][0]['request']['headers']
+    
+    assert 'foo bar' in heads['User-Agent'][0]
+    assert 'foo bar' in heads['X-USER-AGENT'][0]
 
 @raises(TypeError)
 def test_ua_string_errors():
