f2b8382211c6c1ef2e2b6fe66959baf5c8bbbb74
Scott Chamberlain <sckott@fastmail.com>
2023-09-27 06:55:14 -0700

    ruff flag syntax change; docs: ignore import not at top level

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import sys
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(".."))
 
-import habanero
+import habanero  # noqa
 
 # -- General configuration ------------------------------------------------
 
