b2afa2177efd85fcbcfa4df7ed0f66eaa8f5924a
Scott Chamberlain <sckott@fastmail.com>
2023-09-27 06:50:05 -0700

    docs: move import habanero down to see if it fixes docs build

--- docs/conf.py
+++ docs/conf.py
@@ -15,13 +15,12 @@
 import os
 import sys
 
-import habanero
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(".."))
 
+import habanero
 
 # -- General configuration ------------------------------------------------
 
